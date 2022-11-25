4106178176e26cf31f03bc37038ab1521ef590c9
wesqu <wesquel@hotmail.com>
2022-11-25 16:24:58 -0300

    Criação do ticket

--- src/main/java/api/loteria/loteriaapi/controllers/TicketController.java
+++ src/main/java/api/loteria/loteriaapi/controllers/TicketController.java
@@ -1,12 +1,35 @@
 package api.loteria.loteriaapi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import api.loteria.loteriaapi.models.Ticket;
+import api.loteria.loteriaapi.services.Mysql.TicketService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/ticket")
 public class TicketController {
     
+    @Autowired
+    private TicketService ticketService;
+
+    @GetMapping
+    public Iterable<Ticket> getTickets(){
+       return ticketService.getTickets(); 
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Ticket ticket) {        
+        return ticketService.create(ticket);
+    }
     
+        
 
 }
--- src/main/java/api/loteria/loteriaapi/models/Ticket.java
+++ src/main/java/api/loteria/loteriaapi/models/Ticket.java
@@ -1,11 +1,16 @@
 package api.loteria.loteriaapi.models;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.Type;
 
 import lombok.Data;
 
@@ -22,6 +27,8 @@ public class Ticket {
     @JoinColumn(name="bet_id", nullable = false)
     private Bet bet;
 
-    
+    @Type(type = "json")
+    @Column(nullable = true)
+    private List<Integer> chosenNumbers;
 
 }
