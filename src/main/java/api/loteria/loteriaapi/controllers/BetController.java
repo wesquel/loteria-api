bd2118fb816a9abed2cf19db7a340997557e4415
wesqu <wesquel@hotmail.com>
2022-12-05 11:40:32 -0300

    added raffleNumber param

--- src/main/java/api/loteria/loteriaapi/controllers/BetController.java
+++ src/main/java/api/loteria/loteriaapi/controllers/BetController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
@@ -54,8 +55,11 @@ public class BetController {
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public BetResponse update(@PathVariable Long id,
-                                             @RequestBody @Valid BetRequest betRequest){
+    public BetResponse update(@PathVariable Long id, 
+        @RequestBody @Valid BetRequest betRequest, 
+        @RequestParam(required = false) String raffleNumber
+
+    ){
         return betService.update(id, betRequest);
     }
 
