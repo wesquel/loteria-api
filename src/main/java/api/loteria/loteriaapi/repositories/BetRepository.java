64fe1bb15ce2b98aad4899b548ce2c730c7d749a
wesqu <wesquel@hotmail.com>
2022-12-28 14:21:43 -0300

    unnecessary @repository

--- src/main/java/api/loteria/loteriaapi/repositories/BetRepository.java
+++ src/main/java/api/loteria/loteriaapi/repositories/BetRepository.java
@@ -1,11 +1,9 @@
 package api.loteria.loteriaapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import api.loteria.loteriaapi.models.Bet;
 
-@Repository
 public interface BetRepository extends JpaRepository<Bet, Long> {
     
 }
--- src/main/java/api/loteria/loteriaapi/repositories/TicketRepository.java
+++ src/main/java/api/loteria/loteriaapi/repositories/TicketRepository.java
@@ -1,11 +1,9 @@
 package api.loteria.loteriaapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import api.loteria.loteriaapi.models.Ticket;
 
-@Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     
 }
